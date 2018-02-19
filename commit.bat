afee6077d45d85aa4eacc829b42fbf695587f49f
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-19 09:07:18 -0500

    update

--- commit.bat
+++ commit.bat
@@ -1,6 +1,5 @@
 git config --global user.email 33796301+OrigamiYoda729@users.noreply.github.com
 git config --global user.name OrigamiYoda729
-cd ../
 git init
 git pull https://github.com/OrigamiYoda729/lightfoot.git master
 git add --all
