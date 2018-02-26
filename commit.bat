8dae216d82a5b231adcdbd993e01ac15b064a3dd
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-26 15:16:38 -0500

    Update: update commit system

--- commit.bat
+++ commit.bat
@@ -1,11 +1,17 @@
 @echo off
 set /p update="Enter Changes: "
-clr
+cls
 git config --global user.email 33796301+OrigamiYoda729@users.noreply.github.com
+cls
 git config --global user.name OrigamiYoda729
-@echo on
+cls
 git init
+cls
 git pull https://github.com/OrigamiYoda729/lightfoot.git master
+cls
 git add --all
+cls
 git commit -m "Update: %update%"
+cls
+@echo on
 git push https://github.com/OrigamiYoda729/lightfoot.git master
