00b6d378f55654f5f71ee6c0beb4907f04c7f596
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-01 21:36:34 -0500

    Update: quizizz

---
+++ quizizz/print.js
@@ -0,0 +1,202 @@
+
+	function print(game) {
+		error = "true";
+		
+		// mutlimedia
+			if (game == "multimedia-1") {   // lesson 1
+				error = "false";
+				return "262817";
+			} else
+			if (game == "multimedia-2") {   // lesson 2
+				error = "nocode";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "multimedia-3") {   // lesson 2
+				error = "nocode";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "multimedia-r1") {   // lesson v1
+				error = "false";
+				return "149988";
+			} else
+			if (game == "multimedia-r2") {   // review v2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+		
+		// programming
+			if (game == "programming-1") {   // lesson 1
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-2") {   // lesson 2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-3") {   // lesson 3
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-4") {   // lesson 4
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-5") {   // lesson 5
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-6") {   // lesson 6
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "programming-r1") {   // lesson v1
+				error = "false";
+				return "376800";
+			} else
+			if (game == "programming-r2") {   // review v2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+		
+		// web design
+			if (game == "webdesign-1") {   // lesson 1
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "webdesign-2") {   // lesson 2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "webdesign-3") {   // lesson 3
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "webdesign-4") {   // lesson 4
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "webdesign-r1") {   // lesson v1
+				error = "false";
+				return "414398";
+			} else
+			if (game == "webdesign-r2") {   // review v2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+		
+		// gaming
+			if (game == "gaming-1") {   // lesson 1
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "gaming-2") {   // lesson 2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "gaming-3") {   // lesson 3
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "gaming-4") {   // lesson 4
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "gaming-r1") {   // review v1
+				error = "false";
+				return "614004";
+			} else
+			if (game == "gaming-r2") {   // review v2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			
+		// iba
+			if (game == "learn-1") {   // lesson 1
+				error = "invalid";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-2") {   // lesson 2
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-3") {   // lesson 3
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-4") {   // lesson 4
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-5") {   // lesson 5
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-6") {   // lesson 6
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-7") {   // lesson 7
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-8") {   // lesson 8
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-9") {   // lesson 9
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-10") {   // lesson 10
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			if (game == "learn-r1") {   // review v1
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+				
+		// extreme
+			if (game == "extreme-1") {   // part 1
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98ab6226147e001acc5cd7";
+			} else
+			if (game == "extreme-2") {   // part 2
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b17a045147001adc1f83";
+			} else
+			if (game == "extreme-3") {   // part 3
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b5ed52714b001b752b5a";
+			} else
+			if (game == "extreme-4") {   // part 4
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b63326147e001acc62a6";
+			} else
+			if (game == "extreme-5") {   // part 5
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b72626147e001acc6340";
+			} else
+			if (game == "extreme-6") {   // part 6
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b79a52714b001b752ccf";
+			} else
+			if (game == "extreme-7") {   // part 7
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b7f626147e001acc63d2";
+			} else
+			if (game == "extreme-8") {   // part 8
+				error = "false";
+				return "https://quizizz.com/print/quiz/5a98b8412b086c001a91a206";
+			} else
+			if (game == "extreme-c1") {   // custom review
+				error = "false";
+				return "https://origamiyoda729.github.io/lightfoot/";
+			} else
+			
+		// error check
+			if (game == "true") {
+				return "false";
+			}
+	
+	}
