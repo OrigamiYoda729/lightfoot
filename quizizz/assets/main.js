dba8c378f1f484734a4daf9f09bc1aef89c8ff81
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-09 09:23:20 -0500

    Update: Quizizz JavaScript

--- quizizz/assets/main.js
+++ quizizz/assets/main.js
@@ -1,4 +1,18 @@
 
+	function proceed(modifier) {
+		var mod = modifier.toLowerCase();
+		if (mod == "next") {
+			document.getElementById("loader").innerHTML = "<img src='assets/loader.svg' style='height: 50px;'>";
+			document.getElementById("proceed-btn").className += " invis-button";
+			setTimeout(function(){ 
+				document.getElementById("loader").innerHTML = "Proceed";
+				document.getElementById("proceed-btn").className = "check-room-btn btn-with-highlighter";
+				document.getElementById("proceed-btn").onclick = "proceed('proceed')";
+			}, 1500);
+		}
+	}
+
 	$(document).ready(function() {
 	  $('select').niceSelect();
 	});
+	
