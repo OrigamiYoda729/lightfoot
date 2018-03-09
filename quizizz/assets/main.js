2bef6c78073815e1f3f7e2354ab2b37f7f2a6e00
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-09 13:02:24 -0500

    Update: Update Quizizz

--- quizizz/assets/main.js
+++ quizizz/assets/main.js
@@ -2,17 +2,24 @@
 	function proceed(modifier) {
 		var mod = modifier.toLowerCase();
 		if (mod == "next") {
+				document.getElementById("proceed-btn").onclick = "";
 			document.getElementById("loader").innerHTML = "<img src='assets/loader.svg' style='height: 50px;'>";
 			document.getElementById("proceed-btn").className += " invis-button";
 			setTimeout(function(){ 
 				document.getElementById("loader").innerHTML = "Proceed";
 				document.getElementById("proceed-btn").className = "check-room-btn btn-with-highlighter";
 				document.getElementById("proceed-btn").onclick = "proceed('proceed')";
-			}, 1500);
+				
+				var select = document.getElementsByClassName("nice-select")[0];
+				var selected = select.getElementsByTagName("span")[0].innerHTML.toLowerCase().split(" ")[1];
+				
+				document.getElementsByClassName(selected)[1].classList.remove("invis");
+			}, 1000);
 		}
 	}
 
 	$(document).ready(function() {
-	  $('select').niceSelect();
+	  $('#main-select').niceSelect();
+	  $('#multimedia-select').niceSelect();
 	});
 	
